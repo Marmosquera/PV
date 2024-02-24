d5713d63b2d1e91745a0abd70b46ed89fb4b4222
Marcelo Mosquera <marcelo.mosquera@teya.com>
2024-02-24 11:01:14 +0000

    Service Tests

---
+++ PV.Pixel/PV.Pixel.Service.Tests/Gif1X1Tests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace PV.Pixel.Service.Tests
+{
+    public class Gif1X1Tests
+    {
+
+        [Fact]
+        public void ToResult_OK()
+        {
+            // Arrange
+            var fileSize = 807;
+
+            // Act
+            var result = Gif1X1.ToResult();  //await _sut.ProducePixelRequested(pixelRequest);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<FileContentHttpResult>(result);
+            var file = result as FileContentHttpResult;
+            Assert.NotNull(file);
+            Assert.Equal(Gif1X1.GIF_CONTENT_TYPE, file.ContentType);
+            Assert.Equal(fileSize, file.FileLength);
+        }
+    }
+}
